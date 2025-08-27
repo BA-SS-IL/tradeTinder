367dd29796f0205d908d35116b6f29450026d695
Basil Varghese <basilvarghese@Basils-MacBook-Air.local>
2025-08-27 14:13:23 +0530

    created express servere

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('hello traders')
+const express = require('express');
+
+const app = express();
+
+app.use('/signUp',(req,res)=>{
+    res.send('welcome to signUp page')
+})
+
+app.listen(3000,()=>{
+    console.log('server is successfully listening port 3000')
+})
